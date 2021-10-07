c160dc2336fc48459a4ed2b10c5b26ee31be65cb
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-10-07 17:15:54 +0300

    added a svg image

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "./style/style.css";
+import image from "./assets/image1.svg";
 
 function App() {
   return (
     <div className="background">
       <div className="container">
-        <h1 className="title">hello world</h1>
+        <h1 className="title">Made by marketing experts</h1>
+        <h1 className="title2">for marketing experts</h1>
+        <img className="img" src={image} alt="" />
       </div>
     </div>
   );
