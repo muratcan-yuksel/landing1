a89d4daf195258433d07076fc326b07e40240a6b
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-10-07 17:39:45 +0300

    fir page will look sth like this

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import "./style/style.css";
 import image from "./assets/image1.svg";
 
@@ -8,9 +6,16 @@ function App() {
   return (
     <div className="background">
       <div className="container">
-        <h1 className="title">Made by marketing experts</h1>
-        <h1 className="title2">for marketing experts</h1>
+        <h1 className="title">Automate your marketing workflow</h1>
+        {/* <h1 className="title2">for marketing experts</h1> */}
         <img className="img" src={image} alt="" />
+        <div className="subContainer">
+          <div className="subCont2">
+            <h3 className="subTitle">Made by professionals</h3>
+            <h3 className="subTitle">for professionals</h3>
+          </div>
+          <h3 className="button">click here</h3>
+        </div>
       </div>
     </div>
   );
